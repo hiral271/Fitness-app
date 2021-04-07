e0defcff16e7771f27b1c2f8d56e502f1a2d4171
Hiral patel <hiralm227@gmail.com>
2021-04-06 21:49:17 -0400

    code error done

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-//set port
+const mongoose = require("mongoose");
+const express = require("express");
+// const { Console } = require("console");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-//use logger
-app.use(logger("dev"));
-
-//parser
+//app.use url json static
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//use static files
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fittrack", {
+
+// bring mongoose.connect
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/fitness',
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true ,
+    useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false});
-
-//require('./seeders/seed')
-
-//use routes
-// require('./routes/api')(app)
-// require('./routes/html')(app)
-app.use(require("./routes/html"));
-app.use(require("./routes/api"));
+    useFindAndModify: false
+  }
+);
+//declare you routes
+app.use(require("./routes/api.js"));
+//declare you api routes
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}..`);
-})
+    console.log(`My app is runningon ${PORT}`)
+});
