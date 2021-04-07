63e8f25985400de14e3b26d5f39031ecb71fc12a
Hiral patel <hiralm227@gmail.com>
2021-04-07 10:26:14 -0400

    routes discription

--- routes/html.js
+++ routes/html.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const path = require('path');
 
+//page for exercise input view
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 
+//main page
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/home.html'));
 });
 
+//states page for graph 
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const express = require("express");
-// const { Console } = require("console");
 
+//port with env for herokuApp
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-//app.use url json static
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 
-// bring mongoose.connect
+
+//mongoose connection
+//same uri key for heroku to add 
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/fitness',
@@ -24,11 +26,11 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
-//declare you routes
+
 app.use(require("./routes/api.js"));
-//declare you api routes
+
 app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
-    console.log(`My app is runningon ${PORT}`)
+    console.log(`This  app is running  on this PORT: ${PORT}`)
 });
